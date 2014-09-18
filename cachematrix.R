79d7a01235b912971963668a1461412ec3731881
ntdatascience <ntdatascience@gmail.com>
2014-09-17 20:54:40 -0700

    programming assigment 2 work

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## ntdatascience
+## purpose: to improve performance on calculating the inverse of the matrix
+##   we will cache matrixes and the corresponding inverse
+## ntdatascience programming assigment 2
 
-## Write a short comment describing this function
+## makeCacheMatrix: creates a custom vector object sot that we can manage caching
+# 1. set/get the matrix
+# 2. set/get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+          x <<- y
+          m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+          setinverse = setinverse,
+          getinverse = getinverse )
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: return the inverse of the matrix
+## if matrix is in cache then return the already calculted inverse
+## otherwise, store the matrix with the calcuted inverse so that it will be cached
+## and then return this calculated inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+      message("cacheSolve")
+      m <- x$getinverse()
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setinverse(m)
+      m
+
 }
